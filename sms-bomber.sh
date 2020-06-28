87852590141d569a6c4ee6933a78070e53a699a3
sokangar <67541281+sokangar@users.noreply.github.com>
2020-06-28 13:08:51 +0300

    Add files via upload

---
+++ sms-bomber.sh
@@ -0,0 +1,4 @@
+pkg install termux-api
+echo PS1='"YA PIDOR\[\033[1;33;1;32m\]:\[\033[1;31m\]\w$ \[\033[0m\]\[\033[0m\]"'>>.bashrc 
+echo -e "SUKA TI BLUAT \SOSI \SOSI \SOSI \SOSI\SOSI"
+termux-toast "OK"
